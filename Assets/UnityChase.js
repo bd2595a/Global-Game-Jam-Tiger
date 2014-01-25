963069b2ade7c244c53da486c6c16ad3fd3f562f
thedrean <at4344a@student.american.edu>
2014-01-25 11:49:12 -0500

    avoiding merge conflict

---
+++ Assets/UnityChase.js
@@ -0,0 +1,30 @@
+﻿#pragma strict
+
+    var player : Transform;
+    var speed : float = 10;
+    var chaseDistance = 150;
+     
+     
+    function Update()
+    {
+     
+     
+     
+     
+    if(Vector3.Distance(transform.position,player.position) <= chaseDistance)
+    {
+    transform.LookAt(player);
+    transform.position += transform.forward*speed*Time.deltaTime;
+     
+     
+     
+    }
+     
+    
+    var distToPlayer = (transform.position - player.position).sqrMagnitude;
+     
+    if( distToPlayer < 5.0 ) {
+    print ("You lost");
+    }
+     
+    }
