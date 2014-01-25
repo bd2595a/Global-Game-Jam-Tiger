473af7e65c71be65401acfab3824ef7ab845b4ba
thedrean <at4344a@student.american.edu>
2014-01-25 11:20:07 -0500

    I made dat tiger

---
+++ Assets/Free-Roam.js
@@ -0,0 +1,28 @@
+﻿#pragma strict
+
+    var moveSpeed : float = 2;
+    var rotSpeed : float = 10;
+    var moveDir : int = 1;
+     
+    function Update()
+    {
+	    if(!Physics.Raycast(transform.position, transform.forward, 5))
+	    {
+	    	transform.Translate(Vector3.forward * moveSpeed * Time.smoothDeltaTime);
+	    }
+	    else
+	    {
+		    if(Physics.Raycast(transform.position, - transform.right, 1))
+		    {
+		    moveDir = 1;
+		    }
+		    else if(Physics.Raycast(transform.position, transform.right, 1))
+		    {
+		    moveDir = -1;
+		    }
+		    transform.Rotate(Vector3.right, 90 * rotSpeed * Time.smoothDeltaTime * moveDir);
+	    }
+    }
+    
+    
+  
---
+++ Assets/WayPointScript.js
@@ -0,0 +1,52 @@
+﻿  
+    var waypoints : Transform[];
+    var speed : int = 2;
+    private var currentWaypoint : int;
+    var loop : boolean = true;
+    var player : Transform;
+     
+    function Start()
+    {
+    player = GameObject.FindWithTag("Player").transform;
+    }
+     
+    function Update()
+    {
+    if(currentWaypoint < waypoints.length)
+    {
+     
+    var target : Vector3 = waypoints[currentWaypoint].position;
+    var moveDirection : Vector3 = target - transform.position;
+    var range : Vector3 = player.position - transform.position;
+     
+    var velocity = rigidbody.velocity;
+    if(moveDirection.magnitude < 1)
+    {
+    currentWaypoint++;
+    }
+    else if(range.magnitude < 10)
+    {
+     
+    velocity = Vector3.zero;
+    target = player.position;
+    }
+    else
+    {
+    velocity = moveDirection.normalized * speed;
+    }
+    }
+    else
+    {
+    if(loop)
+    {
+    currentWaypoint = 0;
+    }
+    else
+    {
+    velocity = Vector3.zero;
+    }
+    }
+    rigidbody.velocity = velocity;
+    transform.LookAt(target);
+    }
+
